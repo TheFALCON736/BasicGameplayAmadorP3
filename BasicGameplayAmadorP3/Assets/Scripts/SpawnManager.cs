723b9bcd50621963511949a4cfc9579b78c30b49
TheFALCON736 <angel7amador06@gmail.com>
2023-03-10 10:31:06 -0800

    hi
    hi

--- BasicGameplayAmadorP3/Assets/Scripts/SpawnManager.cs
+++ BasicGameplayAmadorP3/Assets/Scripts/SpawnManager.cs
@@ -9,7 +9,10 @@ public class SpawnManager : MonoBehaviour
     private float spawnPosZ = 20;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
-    
+    public float sideSpawnMinZ;
+    public float sideSpawnMaxZ;
+    public float sideSpawnX;
+
     // Start is called before the first frame update
     void Start()
     {
